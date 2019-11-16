6d71542d0d896740348177a07b03a6fac0c10b3f
yagotome <yagotome@gmail.com>
2019-11-16 17:50:01 -0300

    Add more operations on node client

--- client/index.js
+++ client/index.js
@@ -2,13 +2,7 @@ const grpc = require('grpc');
 const protoLoader = require('@grpc/proto-loader');
 const { promisify } = require('util');
 
-const packageDefinition = protoLoader.loadSync(
-    '../cache.proto',
-    {
-        keepCase: true,
-    },
-);
-
+const packageDefinition = protoLoader.loadSync('../cache.proto');
 
 const { cachepb } = grpc.loadPackageDefinition(packageDefinition);
 const { CacheService } = cachepb;
@@ -20,4 +14,12 @@ const storeItem = promisify(client.Store.bind(client));
 (async () => {
     const item = await getItem({ key: 'color' });
     console.log('returned item:', item);
+    
+    await storeItem({ key: 'color', value: 'red' });
+    await storeItem({ key: 'champion', value: 'Flamengo' });
+
+    const { value: color } = await getItem({ key: 'color' });
+    const { value: champion } = await getItem({ key: 'champion' });
+    console.log('returned color:', color);
+    console.log('returned champion:', champion);
 })();
